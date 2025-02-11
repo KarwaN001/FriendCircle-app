dc14dd39ef7ebb6bc470f04f1f69523efd89774f
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-02-12 02:57:12 +0300

    channels updated

--- back/routes/channels.php
+++ back/routes/channels.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Group;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('group.{groupId}', function ($user, $groupId) {
+    $group = Group::find($groupId);
+    return $group && $group->members()->where('user_id', $user->id)->exists();
 });
