b561a651a1375f55c0e27a20705b1f3b2863e8e4
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-02-12 01:48:23 +0300

    MessageDeleted event implemented

---
+++ back/app/Events/MessageDeleted.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageDeleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public int $messageId,
+        public int $groupId
+    ) {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('group.'.$this->groupId)
+        ];
+    }
+}
